de5b0ae221e6a88ac226505545608c3f45f3cf37
kabradshaw1 <itllbok82@gmail.com>
2023-01-04 13:42:01 -0500

    added button to save results

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -9,6 +9,9 @@ const Results = () => {
       <div className="card-body">
         <p className='pill mb-3'>Stuff we searched for?</p>
       </div>
+      <button className="btn col-12 col-md-3" type="submit" id='save_park'>
+        Save Results
+      </button>
     </div>
   )
 }
