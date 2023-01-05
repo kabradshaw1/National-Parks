407dceed26afdebed1d65518c363a62ab5604e09
Ashley Steele Mullikin <109765172+ashleymullikin@users.noreply.github.com>
2023-01-04 20:20:49 -0500

    Update to log in

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
-import Auth from '../utils/auth';
+import Auth from '../utils/auth.js';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
