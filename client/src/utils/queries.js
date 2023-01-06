3c215c9d4759e00d432e2fab327658bb5423215f
kabradshaw1 <itllbok82@gmail.com>
2023-01-06 15:28:18 -0500

    removed commented out code, with server still start

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,78 @@
-// import { gql } from '@apollo/client';
-// // This file currently has all the content of both the book app and deep thoughts app.  They are labelled and slightly seperated
+import { gql } from '@apollo/client';
+// This file currently has all the content of both the book app and deep thoughts app.  They are labelled and slightly seperated
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      saved {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_SAVED = gql`
+  query saved($id: ID!) {
+    saved(_id: &id) {
+      _id
+      name
+      username
+      createdAt
+    }
+  }
+`;
+
+
+// Deep Thoughts
+// export const QUERY_THOUGHTS = gql`
+//   query thoughts($username: String) {
+//     thoughts(username: $username) {
+//       _id
+//       thoughtText
+//       createdAt
+//       username
+//       reactionCount
+//       reactions {
+//         _id
+//         createdAt
+//         username
+//         reactionBody
+//       }
+//     }
+//   }
+// `;
+
+// export const QUERY_THOUGHT = gql`
+//   query thought($id: ID!) {
+//     thought(_id: $id) {
+//       _id
+//       thoughtText
+//       createdAt
+//       username
+//       reactionCount
+//       reactions {
+//         _id
+//         createdAt
+//         username
+//         reactionBody
+//       }
+//     }
+//   }
+// `;
 
 // export const QUERY_USER = gql`
 //   query user($username: String!) {
@@ -7,6 +80,17 @@
 //       _id
 //       username
 //       email
+//       friendCount
+//       friends {
+//         _id
+//         username
+//       }
+//       thoughts {
+//         _id
+//         thoughtText
+//         createdAt
+//         reactionCount
+//       }
 //     }
 //   }
 // `;
@@ -17,9 +101,37 @@
 //       _id
 //       username
 //       email
-//       saved {
+//       friendCount
+//       thoughts {
 //         _id
-//         name
+//         thoughtText
+//         createdAt
+//         reactionCount
+//         reactions {
+//           _id
+//           createdAt
+//           reactionBody
+//           username
+//         }
+//       }
+//       friends {
+//         _id
+//         username
+//       }
+//     }
+//   }
+// `;
+
+// export const QUERY_ME_BASIC = gql`
+//   {
+//     me {
+//       _id
+//       username
+//       email
+//       friendCount
+//       friends {
+//         _id
+//         username
 //       }
 //     }
 //   }
@@ -27,125 +139,22 @@
 
 
 
-
-// // Deep Thoughts
-// // export const QUERY_THOUGHTS = gql`
-// //   query thoughts($username: String) {
-// //     thoughts(username: $username) {
-// //       _id
-// //       thoughtText
-// //       createdAt
-// //       username
-// //       reactionCount
-// //       reactions {
-// //         _id
-// //         createdAt
-// //         username
-// //         reactionBody
-// //       }
-// //     }
-// //   }
-// // `;
-
-// // export const QUERY_THOUGHT = gql`
-// //   query thought($id: ID!) {
-// //     thought(_id: $id) {
-// //       _id
-// //       thoughtText
-// //       createdAt
-// //       username
-// //       reactionCount
-// //       reactions {
-// //         _id
-// //         createdAt
-// //         username
-// //         reactionBody
-// //       }
-// //     }
-// //   }
-// // `;
-
-// // export const QUERY_USER = gql`
-// //   query user($username: String!) {
-// //     user(username: $username) {
-// //       _id
-// //       username
-// //       email
-// //       friendCount
-// //       friends {
-// //         _id
-// //         username
-// //       }
-// //       thoughts {
-// //         _id
-// //         thoughtText
-// //         createdAt
-// //         reactionCount
-// //       }
-// //     }
-// //   }
-// // `;
-
-// // export const QUERY_ME = gql`
-// //   {
-// //     me {
-// //       _id
-// //       username
-// //       email
-// //       friendCount
-// //       thoughts {
-// //         _id
-// //         thoughtText
-// //         createdAt
-// //         reactionCount
-// //         reactions {
-// //           _id
-// //           createdAt
-// //           reactionBody
-// //           username
-// //         }
-// //       }
-// //       friends {
-// //         _id
-// //         username
-// //       }
-// //     }
-// //   }
-// // `;
-
-// // export const QUERY_ME_BASIC = gql`
-// //   {
-// //     me {
-// //       _id
-// //       username
-// //       email
-// //       friendCount
-// //       friends {
-// //         _id
-// //         username
-// //       }
-// //     }
-// //   }
-// // `;
-
-
-
-// // Book app
-// // export const GET_ME = gql`
-// //   {
-// //     me {
-// //       _id
-// //       username
-// //       email
-// //       bookCount
-// //       savedBooks {
-// //         bookId
-// //         authors
-// //         image
-// //         link
-// //         title
-// //         description
-// //       }
-// //     }
-// //   }
-// // `;
+// Book app
+// export const GET_ME = gql`
+//   {
+//     me {
+//       _id
+//       username
+//       email
+//       bookCount
+//       savedBooks {
+//         bookId
+//         authors
+//         image
+//         link
+//         title
+//         description
+//       }
+//     }
+//   }
+// `;
