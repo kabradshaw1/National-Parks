e20e9c05cbb4e5b22a530515f6a785c64ba43e31
Ashley Steele Mullikin <109765172+ashleymullikin@users.noreply.github.com>
2023-01-09 16:02:10 -0500

    ui update again

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+    <div className="container">
+      &copy;{new Date().getFullYear()} <a href="https://github.com/kabradshaw1" >Kyle Bradshaw, </a> <a href="https://github.com/ashleymullikin" >Ashley Mullikin, </a> <a href="https://github.com/erbeard" >Edward Beard, </a> <a href="https://github.com/landank" >Landon Blankship, </a> <a href="https://github.com/August-Lascola" >August Lascola, </a> <a href="https://github.com/kabradshaw1" >Aaron Fridale</a>
+    </div>
+  </footer>
+  );
+};
+
+export default Footer;
